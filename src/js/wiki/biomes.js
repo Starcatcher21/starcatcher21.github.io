fd61351b37fbef2777f7860fbd862dc4bebe0dcc
wilwe <wilwe036@gmail.com>
2026-05-26 20:43:51 +0200

    wiki

--- src/js/wiki/biomes.js
+++ src/js/wiki/biomes.js
@@ -10,10 +10,10 @@ export default function Biomes() {
 			<h2>Stargazer biomes</h2>
 		</div>
 		<div className='pages center'>
-		<Window name="Moon Forest" rout="/#/wiki/biomes/moon_forest" min='/wiki' full><img src={mf} /></Window>
-		<Window name="Bone Forest" rout="/#/wiki/biomes/bone_forest" min='/wiki' full><img src={bf} /></Window>
-		<Window name="Moon Wastes" rout="/#/wiki/biomes/moon_wastes" min='/wiki' full><img src={mw}/></Window>
-		<Window name="Amethyst Spikes" rout="/#/wiki/biomes/amethyst_spikes" min='/wiki' full><img src={asp}/></Window>
+		<Window name="Moon Forest" rout="/wiki/biomes/moon_forest" min='/wiki' full><img src={mf} /></Window>
+		<Window name="Bone Forest" rout="/wiki/biomes/bone_forest" min='/wiki' full><img src={bf} /></Window>
+		<Window name="Moon Wastes" rout="/wiki/biomes/moon_wastes" min='/wiki' full><img src={mw}/></Window>
+		<Window name="Amethyst Spikes" rout="/wiki/biomes/amethyst_spikes" min='/wiki' full><img src={asp}/></Window>
 		</div>
 		</>
 	)
--- src/js/wiki/index.js
+++ src/js/wiki/index.js
@@ -1,6 +1,11 @@
 import {Link} from 'react-router-dom';
 import Window from '../decorator';
 import mf from './images/biomes/moon_forest.png';
+import mt from './images/Trees/moon_tree.png';
+import pt from './images/Mechanics/portal.png';
+import ght from './images/mobs/ghost.png';
+import ls from './images/craftings/loadstone.png';
+
 export default function wiki() {
 	return (
 		<>
@@ -8,11 +13,11 @@ export default function wiki() {
 			<h2>Welcome to stargazer minecraft mod wiki</h2>
 		</div>
 		<div className='pages center'>
-		<Window name="Biomes" rout="/#/wiki/biomes" min='/wiki' full><Link to="/wiki/biomes"><img src={mf} /></Link></Window>
-		<Window name="Mobs" rout="/#/wiki/mobs" min='/wiki' full></Window>
-		<Window name="Trees" rout="/#/wiki/trees" min='/wiki' full></Window>
-		<Window name="Mechanics" rout="/#/wiki/mechanics" min='/wiki' full></Window>
-		<Window name="Craftings" rout="/#/wiki/Craftings" min='/wiki' full></Window>
+		<Window name="Biomes" rout="/wiki/biomes" min='/wiki' full><Link to="/wiki/biomes"><img src={mf} /></Link></Window>
+		<Window name="Mobs" rout="/wiki/mobs" min='/wiki' full><Link to="/wiki/mobs"><img src={ght} /></Link></Window>
+		<Window name="Trees" rout="/wiki/trees" min='/wiki' full><Link to="/wiki/trees"><img src={mt} /></Link></Window>
+		<Window name="Mechanics" rout="/wiki/mechanics" min='/wiki' full><Link to="/wiki/mechanics"><img src={pt} /></Link></Window>
+		<Window name="Craftings" rout="/wiki/craftings" min='/wiki' full><Link to="/wiki/craftings"><img src={ls} /></Link></Window>
 		</div>
 		</>
 	)
