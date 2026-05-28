f6882a60eefe0d01a3105b51d4fba9a9c2466cdd
wilwe <wilwe036@gmail.com>
2026-05-28 14:24:44 +0200

    bop fix

--- src/imageRegistry.js
+++ src/imageRegistry.js
@@ -2,52 +2,56 @@ import img_emotes_0 from './images/emotes/).png';
 import img_emotes_1 from './images/emotes/3.png';
 import img_emotes_2 from './images/emotes/O.png';
 import img_emotes_3 from './images/emotes/P.png';
-import img_emotes_4 from './images/emotes/lenny.png';
-import img_emotes_5 from './images/emotes/meow.png';
-import img_emotes_6 from './images/emotes/mniejniz3.png';
-import img_emotes_7 from './images/emotes/wink).png';
-import img_emotes_8 from './images/emotes/wink3.png';
-import img_emotes_9 from './images/emotes/winkO.png';
-import img_emotes_10 from './images/emotes/winkP.png';
-import img_photos_11 from './images/photos/awtism.png';
-import img_photos_12 from './images/photos/ballkucexe.png';
-import img_photos_13 from './images/photos/boopkucexe.png';
-import img_photos_14 from './images/photos/boopkucexe_small.png';
-import img_photos_15 from './images/photos/boopych.png';
-import img_photos_16 from './images/photos/bunny_hug.png';
-import img_photos_17 from './images/photos/bunny_hug2.png';
-import img_photos_18 from './images/photos/cosmic.jpg';
-import img_photos_19 from './images/photos/creeksona.jpg';
-import img_photos_20 from './images/photos/dealwithkucexe.png';
-import img_photos_21 from './images/photos/earhood.png';
-import img_photos_22 from './images/photos/eepykucexe.png';
-import img_photos_23 from './images/photos/eepykucexe_small.png';
-import img_photos_24 from './images/photos/flower_get.png';
-import img_photos_25 from './images/photos/glasseskucexe.png';
-import img_photos_26 from './images/photos/jocker.png';
-import img_photos_27 from './images/photos/kucexe.png';
-import img_photos_28 from './images/photos/kucnapadexe.png';
-import img_photos_29 from './images/photos/kucnapadexe_small.png';
-import img_photos_30 from './images/photos/kucswagexe.png';
-import img_photos_31 from './images/photos/matterkucexe.png';
-import img_photos_32 from './images/photos/old_ref.png';
-import img_photos_33 from './images/photos/randewu.png';
-import img_photos_34 from './images/photos/salutkucexe.png';
-import img_photos_35 from './images/photos/stargaze.png';
-import img_photos_36 from './images/photos/teapony.png';
-import img_photos_37 from './images/photos/transkucexe.png';
-import img_photos_38 from './images/photos/transkucexe_small.png';
-import img_w_39 from './images/w/).png';
-import img_w_40 from './images/w/3.png';
-import img_w_41 from './images/w/O.png';
-import img_w_42 from './images/w/P.png';
-import img_w_43 from './images/w/lenny.png';
-import img_w_44 from './images/w/meow.png';
-import img_w_45 from './images/w/mniejniz3.png';
-import img_w_46 from './images/w/wink).png';
-import img_w_47 from './images/w/wink3.png';
-import img_w_48 from './images/w/winkO.png';
-import img_w_49 from './images/w/winkP.png';
+import img_emotes_4 from './images/emotes/bop.gif';
+import img_emotes_5 from './images/emotes/lenny.png';
+import img_emotes_6 from './images/emotes/meow.png';
+import img_emotes_7 from './images/emotes/mniejniz3.png';
+import img_emotes_8 from './images/emotes/wink).png';
+import img_emotes_9 from './images/emotes/wink3.png';
+import img_emotes_10 from './images/emotes/winkO.png';
+import img_emotes_11 from './images/emotes/winkP.png';
+import img_photos_12 from './images/photos/awtism.png';
+import img_photos_13 from './images/photos/ballkucexe.png';
+import img_photos_14 from './images/photos/boopkucexe.png';
+import img_photos_15 from './images/photos/boopkucexe_small.png';
+import img_photos_16 from './images/photos/boopych.png';
+import img_photos_17 from './images/photos/bunny_hug.png';
+import img_photos_18 from './images/photos/bunny_hug2.png';
+import img_photos_19 from './images/photos/cosmic.jpg';
+import img_photos_20 from './images/photos/creeksona.jpg';
+import img_photos_21 from './images/photos/dance.gif';
+import img_photos_22 from './images/photos/dealwithkucexe.png';
+import img_photos_23 from './images/photos/earhood.png';
+import img_photos_24 from './images/photos/eepykucexe.png';
+import img_photos_25 from './images/photos/eepykucexe_small.png';
+import img_photos_26 from './images/photos/flower_get.png';
+import img_photos_27 from './images/photos/glasseskucexe.png';
+import img_photos_28 from './images/photos/japan.gif';
+import img_photos_29 from './images/photos/jocker.png';
+import img_photos_30 from './images/photos/kucexe.png';
+import img_photos_31 from './images/photos/kucnapadexe.png';
+import img_photos_32 from './images/photos/kucnapadexe_small.png';
+import img_photos_33 from './images/photos/kucswagexe.png';
+import img_photos_34 from './images/photos/matterkucexe.png';
+import img_photos_35 from './images/photos/old_ref.png';
+import img_photos_36 from './images/photos/randewu.png';
+import img_photos_37 from './images/photos/salutkucexe.png';
+import img_photos_38 from './images/photos/stargaze.png';
+import img_photos_39 from './images/photos/teapony.png';
+import img_photos_40 from './images/photos/transkucexe.png';
+import img_photos_41 from './images/photos/transkucexe_small.png';
+import img_w_42 from './images/w/).png';
+import img_w_43 from './images/w/3.png';
+import img_w_44 from './images/w/O.png';
+import img_w_45 from './images/w/P.png';
+import img_w_46 from './images/w/bop.gif';
+import img_w_47 from './images/w/lenny.png';
+import img_w_48 from './images/w/meow.png';
+import img_w_49 from './images/w/mniejniz3.png';
+import img_w_50 from './images/w/wink).png';
+import img_w_51 from './images/w/wink3.png';
+import img_w_52 from './images/w/winkO.png';
+import img_w_53 from './images/w/winkP.png';
 
 export const imageRegistry = {
   'emotes': {
@@ -55,55 +59,59 @@ export const imageRegistry = {
     '3.png': img_emotes_1,
     'O.png': img_emotes_2,
     'P.png': img_emotes_3,
-    'lenny.png': img_emotes_4,
-    'meow.png': img_emotes_5,
-    'mniejniz3.png': img_emotes_6,
-    'wink).png': img_emotes_7,
-    'wink3.png': img_emotes_8,
-    'winkO.png': img_emotes_9,
-    'winkP.png': img_emotes_10,
+    'bop.gif': img_emotes_4,
+    'lenny.png': img_emotes_5,
+    'meow.png': img_emotes_6,
+    'mniejniz3.png': img_emotes_7,
+    'wink).png': img_emotes_8,
+    'wink3.png': img_emotes_9,
+    'winkO.png': img_emotes_10,
+    'winkP.png': img_emotes_11,
   },
   'photos': {
-    'awtism.png': img_photos_11,
-    'ballkucexe.png': img_photos_12,
-    'boopkucexe.png': img_photos_13,
-    'boopkucexe_small.png': img_photos_14,
-    'boopych.png': img_photos_15,
-    'bunny_hug.png': img_photos_16,
-    'bunny_hug2.png': img_photos_17,
-    'cosmic.jpg': img_photos_18,
-    'creeksona.jpg': img_photos_19,
-    'dealwithkucexe.png': img_photos_20,
-    'earhood.png': img_photos_21,
-    'eepykucexe.png': img_photos_22,
-    'eepykucexe_small.png': img_photos_23,
-    'flower_get.png': img_photos_24,
-    'glasseskucexe.png': img_photos_25,
-    'jocker.png': img_photos_26,
-    'kucexe.png': img_photos_27,
-    'kucnapadexe.png': img_photos_28,
-    'kucnapadexe_small.png': img_photos_29,
-    'kucswagexe.png': img_photos_30,
-    'matterkucexe.png': img_photos_31,
-    'old_ref.png': img_photos_32,
-    'randewu.png': img_photos_33,
-    'salutkucexe.png': img_photos_34,
-    'stargaze.png': img_photos_35,
-    'teapony.png': img_photos_36,
-    'transkucexe.png': img_photos_37,
-    'transkucexe_small.png': img_photos_38,
+    'awtism.png': img_photos_12,
+    'ballkucexe.png': img_photos_13,
+    'boopkucexe.png': img_photos_14,
+    'boopkucexe_small.png': img_photos_15,
+    'boopych.png': img_photos_16,
+    'bunny_hug.png': img_photos_17,
+    'bunny_hug2.png': img_photos_18,
+    'cosmic.jpg': img_photos_19,
+    'creeksona.jpg': img_photos_20,
+    'dance.gif': img_photos_21,
+    'dealwithkucexe.png': img_photos_22,
+    'earhood.png': img_photos_23,
+    'eepykucexe.png': img_photos_24,
+    'eepykucexe_small.png': img_photos_25,
+    'flower_get.png': img_photos_26,
+    'glasseskucexe.png': img_photos_27,
+    'japan.gif': img_photos_28,
+    'jocker.png': img_photos_29,
+    'kucexe.png': img_photos_30,
+    'kucnapadexe.png': img_photos_31,
+    'kucnapadexe_small.png': img_photos_32,
+    'kucswagexe.png': img_photos_33,
+    'matterkucexe.png': img_photos_34,
+    'old_ref.png': img_photos_35,
+    'randewu.png': img_photos_36,
+    'salutkucexe.png': img_photos_37,
+    'stargaze.png': img_photos_38,
+    'teapony.png': img_photos_39,
+    'transkucexe.png': img_photos_40,
+    'transkucexe_small.png': img_photos_41,
   },
   'w': {
-    ').png': img_w_39,
-    '3.png': img_w_40,
-    'O.png': img_w_41,
-    'P.png': img_w_42,
-    'lenny.png': img_w_43,
-    'meow.png': img_w_44,
-    'mniejniz3.png': img_w_45,
-    'wink).png': img_w_46,
-    'wink3.png': img_w_47,
-    'winkO.png': img_w_48,
-    'winkP.png': img_w_49,
+    ').png': img_w_42,
+    '3.png': img_w_43,
+    'O.png': img_w_44,
+    'P.png': img_w_45,
+    'bop.gif': img_w_46,
+    'lenny.png': img_w_47,
+    'meow.png': img_w_48,
+    'mniejniz3.png': img_w_49,
+    'wink).png': img_w_50,
+    'wink3.png': img_w_51,
+    'winkO.png': img_w_52,
+    'winkP.png': img_w_53,
   },
 };
