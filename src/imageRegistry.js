05aa29db446051cd82fcf2339e2fc942291e072c
wilwe <wilwe036@gmail.com>
2026-05-28 07:49:19 +0200

    images fix

---
+++ src/imageRegistry.js
@@ -0,0 +1,109 @@
+import img_emotes_0 from './images/emotes/).png';
+import img_emotes_1 from './images/emotes/3.png';
+import img_emotes_2 from './images/emotes/O.png';
+import img_emotes_3 from './images/emotes/P.png';
+import img_emotes_4 from './images/emotes/lenny.png';
+import img_emotes_5 from './images/emotes/meow.png';
+import img_emotes_6 from './images/emotes/mniejniz3.png';
+import img_emotes_7 from './images/emotes/wink).png';
+import img_emotes_8 from './images/emotes/wink3.png';
+import img_emotes_9 from './images/emotes/winkO.png';
+import img_emotes_10 from './images/emotes/winkP.png';
+import img_photos_11 from './images/photos/awtism.png';
+import img_photos_12 from './images/photos/ballkucexe.png';
+import img_photos_13 from './images/photos/boopkucexe.png';
+import img_photos_14 from './images/photos/boopkucexe_small.png';
+import img_photos_15 from './images/photos/boopych.png';
+import img_photos_16 from './images/photos/bunny_hug.png';
+import img_photos_17 from './images/photos/bunny_hug2.png';
+import img_photos_18 from './images/photos/cosmic.jpg';
+import img_photos_19 from './images/photos/creeksona.jpg';
+import img_photos_20 from './images/photos/dealwithkucexe.png';
+import img_photos_21 from './images/photos/earhood.png';
+import img_photos_22 from './images/photos/eepykucexe.png';
+import img_photos_23 from './images/photos/eepykucexe_small.png';
+import img_photos_24 from './images/photos/flower_get.png';
+import img_photos_25 from './images/photos/glasseskucexe.png';
+import img_photos_26 from './images/photos/jocker.png';
+import img_photos_27 from './images/photos/kucexe.png';
+import img_photos_28 from './images/photos/kucnapadexe.png';
+import img_photos_29 from './images/photos/kucnapadexe_small.png';
+import img_photos_30 from './images/photos/kucswagexe.png';
+import img_photos_31 from './images/photos/matterkucexe.png';
+import img_photos_32 from './images/photos/old_ref.png';
+import img_photos_33 from './images/photos/randewu.png';
+import img_photos_34 from './images/photos/salutkucexe.png';
+import img_photos_35 from './images/photos/stargaze.png';
+import img_photos_36 from './images/photos/teapony.png';
+import img_photos_37 from './images/photos/transkucexe.png';
+import img_photos_38 from './images/photos/transkucexe_small.png';
+import img_w_39 from './images/w/).png';
+import img_w_40 from './images/w/3.png';
+import img_w_41 from './images/w/O.png';
+import img_w_42 from './images/w/P.png';
+import img_w_43 from './images/w/lenny.png';
+import img_w_44 from './images/w/meow.png';
+import img_w_45 from './images/w/mniejniz3.png';
+import img_w_46 from './images/w/wink).png';
+import img_w_47 from './images/w/wink3.png';
+import img_w_48 from './images/w/winkO.png';
+import img_w_49 from './images/w/winkP.png';
+
+export const imageRegistry = {
+  'emotes': {
+    ').png': img_emotes_0,
+    '3.png': img_emotes_1,
+    'O.png': img_emotes_2,
+    'P.png': img_emotes_3,
+    'lenny.png': img_emotes_4,
+    'meow.png': img_emotes_5,
+    'mniejniz3.png': img_emotes_6,
+    'wink).png': img_emotes_7,
+    'wink3.png': img_emotes_8,
+    'winkO.png': img_emotes_9,
+    'winkP.png': img_emotes_10,
+  },
+  'photos': {
+    'awtism.png': img_photos_11,
+    'ballkucexe.png': img_photos_12,
+    'boopkucexe.png': img_photos_13,
+    'boopkucexe_small.png': img_photos_14,
+    'boopych.png': img_photos_15,
+    'bunny_hug.png': img_photos_16,
+    'bunny_hug2.png': img_photos_17,
+    'cosmic.jpg': img_photos_18,
+    'creeksona.jpg': img_photos_19,
+    'dealwithkucexe.png': img_photos_20,
+    'earhood.png': img_photos_21,
+    'eepykucexe.png': img_photos_22,
+    'eepykucexe_small.png': img_photos_23,
+    'flower_get.png': img_photos_24,
+    'glasseskucexe.png': img_photos_25,
+    'jocker.png': img_photos_26,
+    'kucexe.png': img_photos_27,
+    'kucnapadexe.png': img_photos_28,
+    'kucnapadexe_small.png': img_photos_29,
+    'kucswagexe.png': img_photos_30,
+    'matterkucexe.png': img_photos_31,
+    'old_ref.png': img_photos_32,
+    'randewu.png': img_photos_33,
+    'salutkucexe.png': img_photos_34,
+    'stargaze.png': img_photos_35,
+    'teapony.png': img_photos_36,
+    'transkucexe.png': img_photos_37,
+    'transkucexe_small.png': img_photos_38,
+  },
+  'w': {
+    ').png': img_w_39,
+    '3.png': img_w_40,
+    'O.png': img_w_41,
+    'P.png': img_w_42,
+    'lenny.png': img_w_43,
+    'meow.png': img_w_44,
+    'mniejniz3.png': img_w_45,
+    'wink).png': img_w_46,
+    'wink3.png': img_w_47,
+    'winkO.png': img_w_48,
+    'winkP.png': img_w_49,
+  },
+};
--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,12 +1,31 @@
 import Window from "./decorator";
-import images from "../images/pony";
+import { imageRegistry } from "../imageRegistry";
 
 export default function Gallery() {
+	const emotes = Object.values(imageRegistry.emotes || {});
+	const emotesw = Object.values(imageRegistry.w || {});
+	const photos = Object.values(imageRegistry.photos || {})
 	return (
 		<div className="gallery">
-		{images.map((img, index) => (
+		{photos.map((img, index) => (
         	<div>
-          		<img key={index} src={img} alt={img} />
+				<Window name={Object.keys(imageRegistry.photos || {})[index].split(".")[0]} rout={"./photos/"+Object.keys(imageRegistry.photos || {})[index].split(".")[0]}>
+          		<img className="image" key={index} src={img} alt={img} />
+				</Window>
+        	</div>
+      	))}
+		{emotes.map((img, index) => (
+        	<div>
+				<Window name={Object.keys(imageRegistry.emotes || {})[index].split(".")[0]} rout={"./emotes/"+Object.keys(imageRegistry.emotes || {})[index].split(".")[0]}>
+          		<img className="image" key={index} src={img} alt={img} />
+				</Window>
+        	</div>
+      	))}
+		{emotesw.map((img, index) => (
+        	<div>
+				<Window name={Object.keys(imageRegistry.w || {})[index].split(".")[0]} rout={"./emotes/w/"+Object.keys(imageRegistry.w || {})[index].split(".")[0]}>
+          		<img className="image" key={index} src={img} alt={img} />
+				</Window>
         	</div>
       	))}
 		</div>
